dedd0073621161f084663be43415a4efb65d9bb6
ahmadox1 <alenzi23u@gmail.com>
2025-09-22 09:04:46 +0300

    Add JitPack repository for tess-two

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,21 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io") {
+            content {
+                includeGroup("com.googlecode.tesseract.android")
+            }
+        }
+        maven("https://s01.oss.sonatype.org/content/repositories/releases/") {
+            content {
+                includeGroup("com.googlecode.tesseract.android")
+            }
+        }
+        maven("https://alphacephei.com/maven") {
+            content {
+                includeGroup("ai.vosk")
+            }
+        }
     }
 }
 
